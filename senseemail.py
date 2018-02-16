b0346af0ac1c9bdc709abfc32e4cc3aadda91ff3
DukeOfNewYork <simongraygit@gmail.com>
2018-02-15 22:41:31 -0500

    Email function returns a "Sent" or "X + " Exception

--- senseemail.py
+++ senseemail.py
@@ -1,19 +1,24 @@
 import smtplib
 
-    #This is all web server setup, this has been speficially used with Gmail
-    #msg is the primary temprature alert message while msgTest is just a message sent when pressing down the control stick
-#fromaddr = 'E-Mail Address that the message will be sent to'
-#toaddrs  = 'E-Mail Address that you want to alert'
-#username = 'E-Mail UserName'
-#password = 'E-Mail Password'
-#server = smtplib.SMTP('SMTP address and port',587)
+# This is all web server setup, this has been speficially used with Gmail
+# msg is the primary temprature alert message while msgTest is just a message sent when pressing down the control stick
+fromaddr = 'E-Mail Address that the message will be sent to'
+toaddrs = 'E-Mail Address that you want to alert'
+username = 'E-Mail UserName'
+password = 'E-Mail Password'
+mailserver = 'SMTP address and port'
+
 
 # This works with Gmail but may need to be adjusted for other e-mail salutions
 
 def sendemail(emailMessage):
-    server.ehlo()
-    server.starttls()
-    server.login(username, password)
-    server.sendmail(fromaddr, toaddrs, emailMessage)
-    server.quit()
-
+    try:
+        server = smtplib.SMTP(mailserver)
+        server.ehlo()
+        server.starttls()
+        server.login(username, password)
+        server.sendmail(fromaddr, toaddrs, emailMessage)
+        server.quit()
+        return ("Sent")
+    except Exception as exc:
+        return ("X " + str(exc))
