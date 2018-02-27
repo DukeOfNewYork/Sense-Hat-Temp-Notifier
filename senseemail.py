8cc845a414892d53730c6f1bb8a468aea8f7db3d
Simon Gray <simongraygit@gmail.com>
2018-02-26 20:35:06 -0500

    Set email to disabled by default

--- senseemail.py
+++ senseemail.py
@@ -1,5 +1,8 @@
 import smtplib
 
+# This sets if e-mails are sent or not
+email_enable = False
+
 # This is all web server setup, this has been speficially used with Gmail
 # msg is the primary temprature alert message while msgTest is just a message sent when pressing down the control stick
 fromaddr = 'E-Mail Address that the message will be sent to'
@@ -12,13 +15,16 @@ mailserver = 'SMTP address and port'
 # This works with Gmail but may need to be adjusted for other e-mail salutions
 
 def sendemail(emailMessage):
-    try:
-        server = smtplib.SMTP(mailserver)
-        server.ehlo()
-        server.starttls()
-        server.login(username, password)
-        server.sendmail(fromaddr, toaddrs, emailMessage)
-        server.quit()
-        return ("Sent")
-    except Exception as exc:
-        return ("X " + str(exc))
+    if email_enable:
+        try:
+            server = smtplib.SMTP(mailserver)
+            server.ehlo()
+            server.starttls()
+            server.login(username, password)
+            server.sendmail(fromaddr, toaddrs, emailMessage)
+            server.quit()
+            return ("Sent")
+        except Exception as exc:
+            return ("X " + str(exc))
+    else:
+        return ("Disabled")
